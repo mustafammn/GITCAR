c72e884d3f3e8d748eee21ab97c3c70bd039bf4a
mustafanooristani <mm.nooristani1@gmail.com>
2022-01-26 20:23:50 -0500

    Here is the green car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        CarBasics car = new CarBasics();
-        car.color="red";
-        car.engine="7999cc";
-        car.door="2";
-        car.number="119018";
 
+        CarBasics car = new CarBasics();
+
+        car.color="green";
+        car.number="789";
+        car.engine="544444ccc";
+        car.door="3";
         System.out.println(car.color);
     }
 }
