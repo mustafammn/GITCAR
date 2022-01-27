a05b87e0f1019eaa62ffe8da95cf370cc181814c
mustafanooristani <mm.nooristani1@gmail.com>
2022-01-26 19:56:58 -0500

    The red car is ready for trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         CarBasics car = new CarBasics();
+        car.color="red";
+        car.engine="7999cc";
+        car.door="2";
+        car.number="119018";
 
+        System.out.println(car.color);
     }
 }
